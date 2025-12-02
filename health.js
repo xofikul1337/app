aa9350730371ae310b547aacb5ffa0d26f025673
Shofikul Islam <168334396+xofikul1337@users.noreply.github.com>
2025-12-02 14:47:00 +0600

    Update health.js

--- health.js
+++ health.js
@@ -4,7 +4,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 // 🔐 Supabase backend client – service role key (backend only)
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_KEY;
 
 if (!supabaseUrl || !supabaseServiceRoleKey) {
   console.warn(
